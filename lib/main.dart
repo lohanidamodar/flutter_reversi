094a89e69248d5d15b90e225815c6715d7e76ce9
Damodar Lohani <dlohani48@gmail.com>
2019-11-21 05:11:43 +0545

    general app

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,64 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _controller;
+  String _reversed;
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Reversi'),
       ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter string to reverse"),
+            ),
+            const SizedBox(height: 10.0),
+            if (_reversed != null) ...[
+              Text(
+                _reversed,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+              const SizedBox(height: 10.0),
+            ],
+            RaisedButton(
+              child: Text("Reverse"),
+              onPressed: () {
+                if (_controller.text.isEmpty) return;
+                setState(() {
+                  _reversed = reverseString(_controller.text);
+                });
+              },
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+String reverseString(String initial) {
+  return initial.split('').reversed.join();
 }
